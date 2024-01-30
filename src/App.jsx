68e18749ad77bb1955ea43453a901ae873a6b7bb
Ajit Goud <ajitgoud.developer@gmail.com>
2024-01-30 19:31:14 +0530

    added basename in router

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,25 @@ import Blogs from "./pages/Blogs.jsx";
 import Blog from "./pages/Blog.jsx";
 import Contact from "./pages/Contact.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/projects", element: <Projects /> },
-      { path: "/contact", element: <Contact /> },
-      // { path: "/projects/:projectId", element: <Project /> },
-      // { path: "/blogs", element: <Blogs /> },
-      // { path: "/blogs/:blogId", element: <Blog /> },
-      { path: "/*", element: <div>Invalid page</div> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/about", element: <About /> },
+        { path: "/projects", element: <Projects /> },
+        { path: "/contact", element: <Contact /> },
+        // { path: "/projects/:projectId", element: <Project /> },
+        // { path: "/blogs", element: <Blogs /> },
+        // { path: "/blogs/:blogId", element: <Blog /> },
+        { path: "/*", element: <div>Invalid page</div> },
+      ],
+    },
+  ],
+  { basename: "https://ajitgoud.github.io/" }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
